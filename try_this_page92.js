55ee7f9415a47abf03b3edc547b8c7b12774e57b
Orca <wbjung2917@gmail.com>
2022-11-02 17:25:38 +0900

    update trythispage92

---
+++ try_this_page92.js
@@ -0,0 +1,17 @@
+const A = [1, 2, 3, 4, 5, 3];
+const B = [1, 22, 3, 44, 5];
+const C = [11, 222, 3, 4, 555];
+const intersect = (set1, set2) =>
+  new Set(set1.filter((e) => new Set(set2).has(e)));
+console.log(intersect(A, B)); // 1, 3, 5
+console.log(intersect(A, C)); // 3, 4
+
+const diff = (set1, set2) => new Set(set1.filter((e) => !new Set(set2).has(e)));
+console.log(diff(A, B)); // 2, 4
+console.log(diff(B, A)); // 22, 44
+console.log(diff(A, C)); // 1, 2, 5
+console.log(diff(B, C)); // 1, 22, 44, 5
+
+const union = (set1, set2) => new Set([...set1, ...set2]);
+console.log(union(A, B)); // 1, 2, 3, 4, 5, 22, 44
+console.log(union(A, C)); // 1, 2, 3, 4, 5, 11, 222, 555
