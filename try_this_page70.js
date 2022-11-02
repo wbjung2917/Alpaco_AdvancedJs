2e0fec5ad6d0dd484cb3611a81f9d96fea6a4967
Orca <wbjung2917@gmail.com>
2022-11-02 14:33:39 +0900

    update trythispage70

--- try_this_page70.js
+++ try_this_page70.js
@@ -1,3 +1,9 @@
 const arr = [1, 2, -3, -4, 5];
 
-console.log(arr.map((x) => Math.sqrt(x ** 2) ** 3));
+const square = (n) => {
+  return (num) => num ** n;
+};
+
+const functions = [square(2), Math.sqrt, square(3)];
+//console.log(arr.map((x) => Math.sqrt(x ** 2) ** 3));
+console.log(functions.reduce((pre, f) => pre.map(f), arr));
