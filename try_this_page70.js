a6f908635a649788eaadad39bd2e654fda10709e
Orca <wbjung2917@gmail.com>
2022-11-02 14:38:29 +0900

    update trythispage70

--- try_this_page70.js
+++ try_this_page70.js
@@ -5,7 +5,8 @@ const square = (n) => {
 };
 
 const functions = [square(2), Math.sqrt, square(3)];
-//console.log(arr.map((x) => Math.sqrt(x ** 2) ** 3));
 const miniRPA = (f_list, init) => f_list.reduce((pre, f) => pre.map(f), init);
 const result = miniRPA(functions, arr);
 console.log(result);
+
+//함수를 바꿔가며 진행할 수 있는가?
