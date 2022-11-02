f4cc859930a1caddae84af88e7e6434d8e6c0e45
Orca <wbjung2917@gmail.com>
2022-11-02 11:14:04 +0900

    update trythispage55

--- try_this_page55.js
+++ try_this_page55.js
@@ -1,16 +1,17 @@
 const weeks = ["일", "월", "화", "수", "목", "금", "토"];
 
 const getNextWeek = () => {
-  if (this.widx === undefined) {
-    this.widx = -1;
-  }
-  this.widx += 1;
-  if (this.widx >= weeks.length) this.widx = 0;
-  return `${weeks[this.widx]}요일`;
+  let widx = -1;
+  return () => {
+    widx += 1;
+    if (widx >= weeks.length) widx = 0;
+    return `${weeks[widx]}요일`;
+  };
 };
 
 let cnt = 0;
+const nextWeek = getNextWeek();
 const intl = setInterval(() => {
-  console.log("call", cnt, getNextWeek());
+  console.log("call", cnt, nextWeek());
   if ((cnt += 1) === 10) clearInterval(intl);
 }, 1000);
