19636c7a7920d1804efd3a9869a61f024e908ad7
Orca <wbjung2917@gmail.com>
2022-11-02 16:28:35 +0900

    update trythispage70,88

--- try_this_page70.js
+++ try_this_page70.js
@@ -1,11 +1,12 @@
-const arr = [1, 2, -3, -4, 5];
-
 const square = (n) => {
   return (num) => num ** n;
 };
 
-const functions = [square(2), Math.sqrt, square(3)];
 const miniRPA = (f_list, init) => f_list.reduce((pre, f) => pre.map(f), init);
+
+const functions = [square(2), Math.sqrt, square(3), square(4)];
+const arr = [1, 2, -3, -4, 5];
+
 const result = miniRPA(functions, arr);
 console.log(result);
 
