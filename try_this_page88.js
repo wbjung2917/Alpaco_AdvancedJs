4d5e250c55a43d135e1865cabfdf879ec111f06d
Orca <wbjung2917@gmail.com>
2022-11-02 16:29:43 +0900

    update trythispage88

--- try_this_page88.js
+++ try_this_page88.js
@@ -20,7 +20,7 @@ for (const emp of emps) {
   empMap.set(emp, deptMap.get(emp.dept));
 }
 
-console.log(deptMap); // Map(2) { '인사팀' => { id: 1, dname: '인사팀' }, '개발팀' => { id: 2, dname: '개발팀' } }
+console.log(deptMap); // Map(2) { 1 => { id: 1, dname: '인사팀' }, 2 => { id: 2, dname: '개발팀' } }
 console.log(empMap); // Map(4) { { id: 1, name: 'Hong' } => { id: 1, dname: '인사팀' }, { id: 2, name: 'Kim' } => { id: 2, dname: '개발팀' }, { id: 3, name: 'Park' } => { id: 2, dname: '개발팀' }, { id: 4, name: 'Choi' } => { id: 2, dname: '개발팀' } }
 
 console.log(empMap.get(kim).dname); // '개발팀'
